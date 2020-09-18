c5611cbd76eda177242b8b691b53aa4a37c6541f
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-09-18 16:52:37 -0400

    Added new, renamed project

---
+++ LegendOfZelda/SetStillSpriteCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0
+{
+    class SetStillSpriteCommand : ICommand
+    {
+        private Game1 myGame;
+        public SetStillSpriteCommand(Game1 game)
+        {
+            myGame = game;
+        }
+
+        public void Execute()
+        {
+            myGame.sprite = new NonMovingNonAnimatedSprite(myGame.Content);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSE-3902-Legendary-Legend-of-Zelda-Dungeon
+Team Project for CSE 3902, creating the first dungeon of Legend of Zelda.
