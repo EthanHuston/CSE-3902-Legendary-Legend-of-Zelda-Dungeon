1ee629ed90976aeb9566fbbdd6e93e06a0ffe691
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-09-21 18:21:25 -0400

    Added class for Link

--- LegendOfZelda/Link.cs
+++ LegendOfZelda/Link.cs
@@ -8,5 +8,32 @@ namespace Sprint0
 {
     class Link : IPlayer
     {
+        int health;
+        
+
+        public Link()
+        {
+
+        }
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Heal(int healAmount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TakeDamage(int damage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
