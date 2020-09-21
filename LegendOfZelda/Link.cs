63cc9702d662db43ace270860bfe40efa169bd2e
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-09-21 19:07:09 -0400

    Added private members and constructor for Link

--- LegendOfZelda/Link.cs
+++ LegendOfZelda/Link.cs
@@ -8,5 +8,14 @@ namespace Sprint0
 {
     class Link : IPlayer
     {
+        private int health;
+        private ILinkState state;
+        private Game1 game;
+
+        public Link(Game1 game) {
+            // health = PLAYER_HEALTH_START
+            this.game = game;
+            // state = someInitialStateClass
+        }
     }
 }
