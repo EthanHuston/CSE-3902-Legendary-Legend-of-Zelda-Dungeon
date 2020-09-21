277a49f07f3837f484cef1ac53003f3501fc9917
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-09-21 18:02:53 -0400

    Add player interface AB#3 and link class AB#4

---
+++ LegendOfZelda/IPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0
+{
+    interface IPlayer
+    {
+        void TakeDamage(int damage);
+        void Heal(int healAmount);
+        void Update();
+        void Draw();
+    }
+}
