9e433b75803414e450fb061d0ef48f1e78aeeea5
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-09-22 17:36:59 -0400

    Added more Link States for walking and damaged AB#129, AB#130, AB#131, AB#132, AB#133

--- LegendOfZelda/Link/LinkDamagedState.cs
+++ LegendOfZelda/Link/LinkDamagedState.cs
@@ -1,12 +1,12 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sprint0
+namespace Sprint0.Link
 {
-    interface ILinkState
+    class LinkDamagedState : ILinkState
     {
     }
 }
