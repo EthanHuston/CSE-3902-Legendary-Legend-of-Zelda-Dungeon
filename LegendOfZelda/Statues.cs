1e7743d596e727ac18fc36cf7ed53d70a913049d
Saumyah Patel <saumyah@hotmail.com>
2020-09-22 19:27:22 -0400

    AB#59 Added all teh classes required.

---
+++ LegendOfZelda/Statues.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0
+{
+    class Statues : IInteractiveEnviornment
+    {
+        public void Update()
+        {
+            
+        }
+    }
+}
