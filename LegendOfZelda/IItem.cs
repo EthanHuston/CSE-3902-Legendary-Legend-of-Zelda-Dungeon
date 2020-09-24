c399ded38c725e3ca9a8ee2c56143acd46a885b3
Nathan Murphy <murphy.1769@buckeyemail.osu.edu>
2020-09-23 21:52:09 -0400

    AB#44 Added Item Interface (IItem).

---
+++ LegendOfZelda/IItem.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0
+{
+    interface IItem
+    {
+        void Update();
+
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
