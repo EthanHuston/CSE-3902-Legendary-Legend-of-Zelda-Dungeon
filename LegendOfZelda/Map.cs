14ccb48b10fe8252fab1c35bac42d8bded636ed0
Nathan Murphy <murphy.1769@buckeyemail.osu.edu>
2020-09-24 21:14:11 -0400

    AB#44 Finalized MapSprite class and Map class.

---
+++ LegendOfZelda/Map.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0
+{
+    class Map : IItem
+    {
+        private int itemIndex = 1;
+        private CompassSprite compassSprite;
+        private SpriteBatch sb;
+        public Map(SpriteBatch spriteBatch)
+        {
+            compassSprite = (CompassSprite)SpriteFactory.Instance.CreateMapSprite();
+            sb = spriteBatch;
+        }
+
+        public void itemAction()
+        {
+            if (Sprint2.itemListCount == itemIndex)
+            {
+                compassSprite.Draw(sb, Sprint2.itemX, Sprint2.itemY);
+            }
+        }
+    }
+}
