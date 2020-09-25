384f542753cb03742ab39bc0c4a9c3730010378b
Nathan Murphy <murphy.1769@buckeyemail.osu.edu>
2020-09-24 22:45:13 -0400

    AB#44 Finalized ArrowSprite class and Arrow class.

---
+++ LegendOfZelda/Arrow.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0
+{
+    class Arrow : IItem
+    {
+        private int itemIndex = 9;
+        private ArrowSprite arrowSprite;
+        private SpriteBatch sb;
+        public Arrow(SpriteBatch spriteBatch)
+        {
+            arrowSprite = (ArrowSprite)SpriteFactory.Instance.CreateArrowSprite();
+            sb = spriteBatch;
+        }
+
+        public void itemAction()
+        {
+            if (Sprint2.itemListCount == itemIndex)
+            {
+                arrowSprite.Draw(sb, Sprint2.itemX, Sprint2.itemY);
+            }
+        }
+    }
+}
