60ba430f1475bf13197ea42f16332fdaba7b0746
Nathan Murphy <murphy.1769@buckeyemail.osu.edu>
2020-09-25 14:29:42 -0400

    AB#44 Updated RupeeSprite class with animation.

--- LegendOfZelda/RupeeSprite.cs
+++ LegendOfZelda/RupeeSprite.cs
@@ -12,19 +12,44 @@ namespace Sprint0
     class RupeeSprite : ISprite
     {
         private Texture2D sprite;
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        private int currentFrame;
+        private int bufferFrame;
+        private int totalFrames;
         public RupeeSprite(Texture2D sprite)
         {
             this.sprite = sprite;
+            Rows = 2;
+            Columns = 1;
+            currentFrame = 0;
+            bufferFrame = 0;
+            totalFrames = Rows * Columns;
         }
         public void Update()
         {
-            // No update needed for an unchanging object.
+            bufferFrame++;
+            if (bufferFrame == 6)
+            {
+                currentFrame++;
+                bufferFrame = 0;
+            }
+            if (currentFrame == totalFrames)
+            {
+                currentFrame = 0;
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch, int XValue, int YValue)
         {
-            Rectangle destinationRectangle = new Rectangle(XValue, YValue, 2 * sprite.Width, 2 * sprite.Width);
-            Rectangle sourceRectangle = new Rectangle(0, 0, sprite.Width, sprite.Height);
+            int width = sprite.Width / Columns;
+            int height = sprite.Height / Rows;
+            int row = (int)((float)currentFrame / (float)Columns);
+            int column = currentFrame % Columns;
+
+            Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
+            Rectangle destinationRectangle = new Rectangle(XValue, YValue, 2 * width, 2 * height);
+
             spriteBatch.Begin();
             spriteBatch.Draw(sprite, destinationRectangle, sourceRectangle, Color.White);
             spriteBatch.End();
