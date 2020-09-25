686c12c8e62c2e58001b7841bd3caaa53bf0b02e
Nathan Murphy <murphy.1769@buckeyemail.osu.edu>
2020-09-24 21:56:03 -0400

    AB#44 Finalized TriforceSprite class and Triforce class.

---
+++ LegendOfZelda/Triforce.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0
+{
+    class Triforce : IItem
+    {
+        private int itemIndex = 4;
+        private TriforceSprite triforceSprite;
+        private SpriteBatch sb;
+        public Triforce(SpriteBatch spriteBatch)
+        {
+            triforceSprite = (TriforceSprite)SpriteFactory.Instance.CreateTriforceSprite();
+            sb = spriteBatch;
+        }
+
+        public void itemAction()
+        {
+            if (Sprint2.itemListCount == itemIndex)
+            {
+                triforceSprite.Draw(sb, Sprint2.itemX, Sprint2.itemY);
+            }
+        }
+    }
+}
