2492bba926aa7c11a4b46932d6dfb3f723033baf
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-09-27 10:54:27 -0400

    Add logic to update damaged Link to flash red
    AB#146

--- LegendOfZelda/IdleDamagedLinkSprite.cs
+++ LegendOfZelda/IdleDamagedLinkSprite.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Sprint0
@@ -10,18 +11,33 @@ namespace Sprint0
     class IdleDamagedLinkSprite : ISprite
     {
         private Texture2D sprite;
+        private int bufferFrame;
+        private int currentFrame;
+        private const int totalFrames = 2;
+        private const int frameDelay = 5;
+        private Color currentColor;
         public IdleDamagedLinkSprite(Texture2D sprite)
         {
             this.sprite = sprite;
         }
         public void Update()
         {
-
+            bufferFrame++;
+            if (bufferFrame == frameDelay)
+            {
+                currentFrame++;
+                bufferFrame = 0;
+            }
+            if (currentFrame == totalFrames)
+            {
+                currentFrame = 0;
+            }
+            currentColor = currentFrame == 0 ? Color.White : Color.Red;
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-
+            spriteBatch.Draw(sprite, new Vector2(Constants.Sprint2LinkSpawnX, Constants.Sprint2LinkSpawnY), currentColor);
         }
     }
 }
