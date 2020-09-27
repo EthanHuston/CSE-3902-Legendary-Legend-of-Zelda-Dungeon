d38afe77ad4e17d475563a3115f49a51d9043ac7
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-09-27 10:53:33 -0400

    Added constants for Link's spawn location in sprint 2
    AB#146

--- LegendOfZelda/Constants.cs
+++ LegendOfZelda/Constants.cs
@@ -8,8 +8,10 @@ namespace Sprint0
 {
     public static class Constants
     {
-        public static int Number = 69;
-        public static int LinkHealth = 100;
+        public const int Number = 69;
+        public const int LinkHealth = 100;
         public const int LinkDamageEffectTimeMs = 2000;
+        public const int Sprint2LinkSpawnX = 0;
+        public const int Sprint2LinkSpawnY = 0;
     }
 }
