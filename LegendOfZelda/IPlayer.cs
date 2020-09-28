02676127d5ac6f8e1eb8843f0ad5d6e073659154
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-09-27 21:57:55 -0400

    Added new methods for controlling player
    AB#2

--- LegendOfZelda/IPlayer.cs
+++ LegendOfZelda/IPlayer.cs
@@ -2,9 +2,19 @@
 {
     internal interface IPlayer
     {
-        void TakeDamage(int damage);
-        void Heal(int healAmount);
-        void Update();
         void Draw();
+        void Update();
+        void Heal(int healAmount);
+        void TakeDamage(int damage);
+        void SubtractHealth(int damage);
+        void MoveUp();
+        void MoveDown();
+        void MoveLeft();
+        void MoveRight();
+        void StopMoving();
+        void SwordRight();
+        void SwordLeft();
+        void SwordDown();
+        void SwordUp();
     }
 }
