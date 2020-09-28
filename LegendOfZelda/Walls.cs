495da1fd82018bbee5e36ec2314eaf1e17d847fa
Saumyah Patel <saumyah@hotmail.com>
2020-09-28 18:31:41 -0400

    AB #59 finished my classes, ready for code review

--- LegendOfZelda/Walls.cs
+++ LegendOfZelda/Walls.cs
@@ -9,6 +9,15 @@ namespace Sprint0
 {
     class Walls : IInteractiveEnviornment
     {
+        private RoomBorderSprite roomBorderSprite;
+        private SpriteBatch sB;
+        int posX, posY;
+        public Walls(SpriteBatch spriteBatch, int x, int y)
+        {
+            roomBorderSprite = (RoomBorderSprite)SpriteFactory.Instance.CreateRoomBorderSprite();
+            sB = spriteBatch;
+            roomBorderSprite.Draw(sB, Sprint2.ieX, Sprint2.ieY);
+        }
         public void Interaction()
         {
             
