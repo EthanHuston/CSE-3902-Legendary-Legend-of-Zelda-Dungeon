0b73672fd9efbde4e5a425e7206842093d5be7de
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-09-29 12:11:18 -0400

    Removed Link sprites because those are in LinkSpriteFactory now
    AB#147; SpriteFactory.cs calls Link.LinkSpriteFactory.LoadAllContent to load Link's content

--- LegendOfZelda/SpriteFactory.cs
+++ LegendOfZelda/SpriteFactory.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Sprint0.Link;
 using Sprint0.Link.Sprite;
 
 namespace Sprint0
 {
     //Sprite Factory based of model found in slides
-    class LinkSpriteFactory
+    class SpriteFactory
     {
         private Texture2D batSprite;
         private Texture2D dogLikeMonsterSprite;
@@ -33,38 +34,17 @@ namespace Sprint0
         private Texture2D mapSprite;
         private Texture2D rupeeSprite;
         private Texture2D triforceSprite;
-        private Texture2D idleLinkDownSprite;
-        private Texture2D idleLinkRightSprite;
-        private Texture2D idleLinkLeftSprite;
-        private Texture2D idleLinkUpSprite;
-        private Texture2D idleDamagedLinkDownSprite;
-        private Texture2D idleDamagedLinkRightSprite;
-        private Texture2D idleDamagedLinkLeftSprite;
-        private Texture2D idleDamagedLinkUpSprite;
-        private Texture2D strikingDownLinkSprite;
-        private Texture2D strikingUpLinkSprite;
-        private Texture2D strikingLeftLinkSprite;
-        private Texture2D strikingRightLinkSprite;
-        private Texture2D pickingUpItemLinkSprite;
-        private Texture2D usingItemDownLinkSprite;
-        private Texture2D usingItemUpLinkSprite;
-        private Texture2D usingItemRightLinkSprite;
-        private Texture2D usingItemLeftLinkSprite;
-        private Texture2D walkingDownLinkSprite;
-        private Texture2D walkingUpLinkSprite;
-        private Texture2D walkingLeftLinkSprite;
-        private Texture2D walkingRightLinkSprite;
         private Texture2D oldManSprite;
         private Texture2D merchantSprite;
-        private static LinkSpriteFactory instance = new LinkSpriteFactory();
-        public static LinkSpriteFactory Instance
+        private static SpriteFactory instance = new SpriteFactory();
+        public static SpriteFactory Instance
         {
             get
             {
                 return instance;
             }
         }
-        private LinkSpriteFactory()
+        private SpriteFactory()
         { 
         }
 
@@ -99,31 +79,10 @@ namespace Sprint0
             mapSprite = content.Load<Texture2D>("Items/Map");
             rupeeSprite = content.Load<Texture2D>("Items/Ruppee");
             triforceSprite = content.Load<Texture2D>("Items/TriforcePiece");
-            //Load Link Sprites
-            idleLinkDownSprite = content.Load<Texture2D>("Link/IdleLinkDown");
-            idleLinkRightSprite = content.Load<Texture2D>("Link/IdleLinkRight");
-            idleLinkLeftSprite = content.Load<Texture2D>("Link/IdleLinkLeft");
-            idleLinkUpSprite = content.Load<Texture2D>("Link/IdleLinkUp");
-            idleDamagedLinkDownSprite = content.Load<Texture2D>("Link/IdleDamagedLinkDown");
-            idleDamagedLinkRightSprite = content.Load<Texture2D>("Link/IdleDamagedLinkRight");
-            idleDamagedLinkLeftSprite = content.Load<Texture2D>("Link/IdleDamagedLinkLeft");
-            idleDamagedLinkUpSprite = content.Load<Texture2D>("Link/IdleDamagedLinkUp");
-            strikingDownLinkSprite = content.Load<Texture2D>("Link/LinkStrikingDown");
-            strikingLeftLinkSprite = content.Load<Texture2D>("Link/LinkStrikingLeft");
-            strikingRightLinkSprite = content.Load<Texture2D>("Link/LinkStrikingRight");
-            strikingUpLinkSprite = content.Load<Texture2D>("Link/LinkStrikingUp");
-            pickingUpItemLinkSprite = content.Load<Texture2D>("Link/PickingUpItemLink");
-            usingItemDownLinkSprite = content.Load<Texture2D>("Link/LinkUsingItemDown");
-            usingItemUpLinkSprite = content.Load<Texture2D>("Link/LinkUsingItemUp");
-            usingItemRightLinkSprite = content.Load<Texture2D>("Link/LinkUsingItemRight");
-            usingItemLeftLinkSprite = content.Load<Texture2D>("Link/LinkUsingItemLeft");
-            walkingDownLinkSprite = content.Load<Texture2D>("Link/WalkingDownLink");
-            walkingLeftLinkSprite = content.Load<Texture2D>("Link/WalkingLeftLink");
-            walkingRightLinkSprite = content.Load<Texture2D>("Link/WalkingRightLink");
-            walkingUpLinkSprite = content.Load<Texture2D>("Link/WalkingUpLink");
             //Load NPC Sprites
             oldManSprite = content.Load<Texture2D>("NPC/OldMan");
             merchantSprite = content.Load<Texture2D>("NPC/Merchant");
+            LinkSpriteFactory.Instance.LoadAllTextures(content);
         }
 
         public ISprite CreateBatSprite()
@@ -229,142 +188,6 @@ namespace Sprint0
         public ISprite CreateTriforceSprite()
         {
             return new TriforceSprite(triforceSprite);
-        }
-        public ISprite CreateIdleLinkDownSprite()
-        {
-            return new IdleLinkSprite(idleLinkDownSprite);
-        }
-        public ISprite CreateIdleLinkLeftSprite()
-        {
-            return new IdleLinkSprite(idleLinkLeftSprite);
-        }
-        public ISprite CreateIdleLinkRightSprite()
-        {
-            return new IdleLinkSprite(idleLinkRightSprite);
-        }
-        public ISprite CreateIdleLinkUpSprite()
-        {
-            return new IdleDamagedLinkSprite(idleLinkUpSprite);
-        }
-        public ISprite CreateIdleDamagedLinkDownSprite()
-        {
-            return new IdleDamagedLinkSprite(idleLinkDownSprite);
-        }
-        public ISprite CreateIdleDamagedLinkLeftSprite()
-        {
-            return new IdleDamagedLinkSprite(idleLinkLeftSprite);
-        }
-        public ISprite CreateIdleDamagedLinkRightSprite()
-        {
-            return new IdleDamagedLinkSprite(idleLinkRightSprite);
-        }
-        public ISprite CreateIdleDamagedLinkUpSprite()
-        {
-            return new IdleDamagedLinkSprite(idleLinkUpSprite);
-        }
-        public ISprite CreateStrikingDownLinkSprite()
-        {
-            return new StrikingLinkSprite(strikingDownLinkSprite);
-        }
-        public ISprite CreateStrikingLeftLinkSprite()
-        {
-            return new StrikingLeftLinkSprite(strikingLeftLinkSprite);
-        }
-        public ISprite CreateStrikingRightLinkSprite()
-        {
-            return new StrikingRightLinkSprite(strikingRightLinkSprite);
-        }
-        public ISprite CreateStrikingUpLinkSprite()
-        {
-            return new StrikingUpLinkSprite(strikingUpLinkSprite);
-        }
-        public ISprite CreatePickingUpItemLinkSprite()
-        {
-            return new PickingUpItemLinkSprite(pickingUpItemLinkSprite);
-        }
-        public ISprite CreatePickingUpItemDamagedLinkSprite()
-        {
-            return new PickingUpItemDamagedLinkSprite(pickingUpItemLinkSprite);
-        }
-        public ISprite CreateUsingItemLeftLinkSprite()
-        {
-            return new UsingItemLinkSprite(usingItemLeftLinkSprite);
-        }
-        public ISprite CreateUsingItemRightLinkSprite()
-        {
-            return new UsingItemLinkSprite(usingItemRightLinkSprite);
-        }
-        public ISprite CreateUsingItemUpLinkSprite()
-        {
-            return new UsingItemLinkSprite(usingItemUpLinkSprite);
-        }
-        public ISprite CreateUsingItemDownLinkSprite()
-        {
-            return new UsingItemLinkSprite(usingItemDownLinkSprite);
-        }
-        public ISprite CreateUsingItemLeftDamagedLinkSprite()
-        {
-            return new UsingItemDamagedLinkSprite(usingItemLeftLinkSprite);
-        }
-        public ISprite CreateUsingItemRightDamagedLinkSprite()
-        {
-            return new UsingItemDamagedLinkSprite(usingItemRightLinkSprite);
-        }
-        public ISprite CreateUsingItemUpDamagedLinkSprite()
-        {
-            return new UsingItemDamagedLinkSprite(usingItemUpLinkSprite);
-        }
-        public ISprite CreateUsingItemDownDamagedLinkSprite()
-        {
-            return new UsingItemDamagedLinkSprite(usingItemDownLinkSprite);
-        }
-        public ISprite CreateWalkingDownLinkSprite()
-        {
-            return new WalkingLinkSprite(walkingDownLinkSprite);
-        }
-        public ISprite CreateWalkingRightLinkSprite()
-        {
-            return new WalkingRightLinkSprite(walkingRightLinkSprite);
-        }
-        public ISprite CreateWalkingLeftLinkSprite()
-        {
-            return new WalkingLeftLinkSprite(walkingLeftLinkSprite);
-        }
-        public ISprite CreateWalkingUpLinkSprite()
-        {
-            return new WalkingUpLinkSprite(walkingUpLinkSprite);
-        }
-        public ISprite CreateStrikingDownDamagedLinkSprite()
-        {
-            return new StrikingDownDamagedLinkSprite(strikingDownLinkSprite);
-        }
-        public ISprite CreateStrikingLeftDamagedLinkSprite()
-        {
-            return new StrikingLeftDamagedLinkSprite(strikingLeftLinkSprite);
-        }
-        public ISprite CreateStrikingRightDamagedLinkSprite()
-        {
-            return new StrikingRightDamagedLinkSprite(strikingRightLinkSprite);
-        }
-        public ISprite CreateStrikingUpDamagedLinkSprite()
-        {
-            return new StrikingUpDamagedLinkSprite(strikingUpLinkSprite);
-        }
-        public ISprite CreateWalkingDownDamagedLinkSprite()
-        {
-            return new WalkingDownDamagedLinkSprite(walkingDownLinkSprite);
-        }
-        public ISprite CreateWalkingRightDamagedLinkSprite()
-        {
-            return new WalkingRightDamagedLinkSprite(walkingRightLinkSprite);
-        }
-        public ISprite CreateWalkingLeftDamagedLinkSprite()
-        {
-            return new WalkingLeftDamagedLinkSprite(walkingLeftLinkSprite);
-        }
-        public ISprite CreateWalkingUpDamagedLinkSprite()
-        {
-            return new WalkingUpDamagedLinkSprite(walkingUpLinkSprite);
         }
         public ISprite CreateOldManSprite()
         {
