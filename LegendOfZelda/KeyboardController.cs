4958de59f3e0b463e66f59086735bbe4f8ebc18b
Nathan Murphy <murphy.1769@buckeyemail.osu.edu>
2020-09-29 22:04:54 -0400

    AB#9 Created/Finalized ArrowCommand class and KeyboardController class.

--- LegendOfZelda/KeyboardController.cs
+++ LegendOfZelda/KeyboardController.cs
@@ -42,6 +42,7 @@ namespace Sprint0
             this.RegisterCommand(Keys.D1, new HeartContainerCommand(game1));
             this.RegisterCommand(Keys.D2, new TriforcePieceCommand(game1));
             this.RegisterCommand(Keys.D3, new BowCommand(game1));
+            this.RegisterCommand(Keys.D4, new ArrowCommand(game1));
         }
 
         public void RegisterCommand(Keys key, ICommand command)
