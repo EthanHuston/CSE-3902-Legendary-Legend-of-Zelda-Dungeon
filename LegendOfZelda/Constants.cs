323852e41ca94ec753d37e616e470d8cac63a328
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-09-30 08:50:18 -0400

    Added constants for boomerang

--- LegendOfZelda/Constants.cs
+++ LegendOfZelda/Constants.cs
@@ -16,14 +16,23 @@ namespace Sprint0
         public const int LinkPickingUpItemPauseTicks = 5;
         public const int LinkUsingItemPauseTicks = 5;
         public const int LinkStrikingPauseTicks = 5;
+
+        // Link's Items
         public const int ArrowSpawnXOffsetFromLink = 0;
         public const int ArrowSpawnYOffsetFromLink = 6;
         public const int BombSpawnXOffsetFromLink = 0;
         public const int BombSpawnYOffsetFromLink = 6;
         public const int BombDelayBeforeExplosion = 60;
+        public const int BoomerangSpawnXOffsetFromLink = 0;
+        public const int BoomerangSpawnYOffsetFromLink = 6;
+        public const int BoomerangDespawnMaxXFromLink = 15;
+        public const int BoomerangDespawnMinXFromLink = 0;
+        public const int BoomerangDespawnMaxYFromLink = 15;
+        public const int BoomerangDespawnMinYFromLink = 0;
 
 
-        // Game screen
+
+        // Game Screen
         public const int MaxXPos = 800;
         public const int MinXPos = 0;
         public const int MaxYPos = 480;
