41e7a0ee5e4ca6ed14dc54731d2dcd0e28760204
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-10-02 10:45:51 -0400

    Added a function to get distance between two points

--- LegendOfZelda/Utility.cs
+++ LegendOfZelda/Utility.cs
@@ -10,27 +10,8 @@ namespace Sprint0
             return position.X > Constants.MaxXPos || position.X < Constants.MinXPos || position.Y > Constants.MaxYPos || position.Y < Constants.MinYPos;
         }
 
-        public class QuadraticCalculator
-        {
-            private double total;
-            private double termA; // f(x) = **ax** + bx^2 + c
-            private double termB; // f(x) = ax + **bx^2** + c
-            private double termC; // f(x) = ax + bx^2 + **c**
-            
-            public QuadraticCalculator(Vector2 pointA, Vector2 pointB)
-            {
-                // calculate equation for point a
-                double aY = pointA.Y;
-                double aA = pointA.X;
-                double aB = Math.Pow(pointA.X, 2);
-
-                // calculate equation for point b
-                double bY = pointB.Y;
-                double bA = pointB.X;
-                double bB = Math.Pow(pointB.X, 2);
-
-
-            }
+        public static float GetDistance(Vector2 position1, Vector2 position2) {
+            return Math.Sqrt(Math.Exp(position1.X - position2.X, 2) + Math.Exp(position1.Y - position2.Y));
         }
     }
 }
