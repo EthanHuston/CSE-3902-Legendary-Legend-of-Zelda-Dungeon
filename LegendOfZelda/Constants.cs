9b6489f0d3f4e5af42b30687f07f7c140e5072c7
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-10-02 14:04:18 -0400

    Added constants for sword beam

--- LegendOfZelda/Constants.cs
+++ LegendOfZelda/Constants.cs
@@ -31,6 +31,10 @@ namespace Sprint0
         public const int BoomerangDespawnMaxYFromLink = 15;
         public const int BoomerangDespawnMinYFromLink = 0;
         public const int BoomerangMaxDistanceFromLink = 300;
+        public const int SwordBeamSpawnXOffsetFromLink = 6;
+        public const int SwordBeamSpawnYOffsetFromLink = 0;
+        public const int SwordBeamFlyingDistanceInterval = 5;
+        public const int SwordBeamMaxDistanceFromLink = 60; // Only needed for Sprint 2
 
         // Game Screen
         public const int MaxXPos = 800;
@@ -52,7 +56,8 @@ namespace Sprint0
         {
             Arrow,
             Bomb,
-            Boomerang
+            Boomerang,
+            SwordBeam
         }
     }
 }
