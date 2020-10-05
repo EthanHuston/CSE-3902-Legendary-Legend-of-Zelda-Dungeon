7627fcd74bdfef774ad4596a53351a5c35eb777a
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-10-04 21:56:18 -0400

    Added new commands and reset game command

---
+++ LegendOfZelda/ResetGameCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace LegendOfZelda
+{
+    internal class ResetGameCommand : ICommand
+    {
+        private Game1 game1;
+
+        public ResetGameCommand(Game1 game1)
+        {
+            this.game1 = game1;
+        }
+
+        public void Execute()
+        {
+            this.game1.ResetGame();
+        }
+    }
+}
