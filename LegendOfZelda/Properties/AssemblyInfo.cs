33470fc0fd293cca58560da4d37b748e107d7f89
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-10-05 11:31:03 -0400

    Added team to assembly info

--- LegendOfZelda/Properties/AssemblyInfo.cs
+++ LegendOfZelda/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("LegendOfZelda")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ryan Stuckey")]
+[assembly: AssemblyCompany("Team 1")]
 [assembly: AssemblyProduct("LegendOfZelda")]
-[assembly: AssemblyCopyright("Copyright © Ryan Stuckey 2020")]
+[assembly: AssemblyCopyright("Copyright © Team 1 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
