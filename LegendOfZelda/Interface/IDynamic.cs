19f6dc4b97a1b37b7ead664cad9d7a69f16faaa7
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-10-11 12:02:49 -0400

    Added an IDynamic interface
    AB#192

---
+++ LegendOfZelda/Interface/IDynamic.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace LegendOfZelda.Interface
+{
+    interface IDynamic : ISpawnable
+    {
+        Vector2 GetVelocity();
+    }
+}
--- LegendOfZelda/Interface/ISpawnable.cs
+++ LegendOfZelda/Interface/ISpawnable.cs
@@ -6,9 +6,9 @@ namespace LegendOfZelda.Interface
     {
         void Update();
         void Draw();
-        Rectangle GetRectangle();
-        Vector2 GetPosition();
-        void SetPosition(Vector2 position);
         void Move(Vector2 distance);
+        void SetPosition(Vector2 position);
+        Vector2 GetPosition();
+        Rectangle GetRectangle();
     }
 }
