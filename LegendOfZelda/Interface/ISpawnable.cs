240377a2a2a767e8fba976b7b34344ed9ed01fa4
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-10-11 11:15:56 -0400

    Added an ISpawnable interface to be implemented by everything that spawns
    AB#190

---
+++ LegendOfZelda/Interface/ISpawnable.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace LegendOfZelda.Interface
+{
+    interface ISpawnable
+    {
+        void Update();
+        void Draw();
+        Rectangle GetRectangle();
+        Vector2 GetPosition();
+        void SetPosition(Vector2 position);
+        void Move(Vector2 distance);
+    }
+}
