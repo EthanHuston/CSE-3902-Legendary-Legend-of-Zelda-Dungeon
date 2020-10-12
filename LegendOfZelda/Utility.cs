1bc0d687b355e7d74c7e56dc8b150e6bea26b4b9
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-10-11 21:26:06 -0400

    Switched to Point instead of Vector2
    Point more accurately represents an (X,Y) value in the 2D plane whereas Vector2 is for distances, velocities, etc.

--- LegendOfZelda/Utility.cs
+++ LegendOfZelda/Utility.cs
@@ -5,12 +5,12 @@ namespace LegendOfZelda
 {
     static class Utility
     {
-        public static bool ItemIsOutOfBounds(Vector2 position)
+        public static bool ItemIsOutOfBounds(Point position)
         {
             return position.X > Constants.MaxXPos || position.X < Constants.MinXPos || position.Y > Constants.MaxYPos || position.Y < Constants.MinYPos;
         }
 
-        public static double GetDistance(Vector2 position1, Vector2 position2)
+        public static double GetDistance(Point position1, Point position2)
         {
             return Math.Sqrt(Math.Pow(position1.X - position2.X, 2) + Math.Pow(position1.Y - position2.Y, 2));
         }
