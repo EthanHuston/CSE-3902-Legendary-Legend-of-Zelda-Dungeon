1abd4dcc571dcf558b910662056cfb8e5d5a9142
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-10-17 10:54:17 -0400

    Added a new interface for sprite coming from a texture atlas (like door)
    Added new overloaded method that also takes a Point to define where source rectangle comes from

---
+++ LegendOfZelda/Interface/ITextureAtlasSprite.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LegendOfZelda.Interface
+{
+    interface ITextureAtlasSprite : ISprite
+    {
+        void Draw(SpriteBatch spriteBatch, Point position, Point textureLocation);
+    }
+}
