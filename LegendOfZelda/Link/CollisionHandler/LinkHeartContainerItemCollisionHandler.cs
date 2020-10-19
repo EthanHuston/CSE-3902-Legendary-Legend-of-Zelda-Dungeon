c3c2b33a138029fca2a88f982af5955774412790
Nathan Murphy <murphy.1769@buckeyemail.osu.edu>
2020-10-18 21:42:26 -0400

    AB#163 Created Link Heart Container Collision

---
+++ LegendOfZelda/Link/CollisionHandler/LinkHeartContainerItemCollisionHandler.cs
@@ -0,0 +1,13 @@
+﻿using LegendOfZelda.Interface;
+
+namespace LegendOfZelda.Link.CollisionHandler
+{
+    class LinkHeartContainerItemCollisionHandler : ICollision<IPlayer, IItem>
+    {
+        public void HandleCollison(IPlayer link, IItem heartContainer, Constants.Direction side)
+        {
+            link.PickUpHeartContainer();
+            heartContainer.Despawn();
+        }
+    }
+}
