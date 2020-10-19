00f0202eee2cec31e237c847758be0a1d6bba865
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-10-18 20:27:42 -0400

    Added sample collision handler for link and bow

---
+++ LegendOfZelda/Link/CollisionHandler/LinkBowItemCollisionHandler.cs
@@ -0,0 +1,18 @@
+﻿using LegendOfZelda.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegendOfZelda.Link.CollisionHandler
+{
+    class LinkBowItemCollisionHandler : ICollision
+    {
+        public void HandleCollison<IPlayer, IItem>(IPlayer link, IItem bow, Constants.Direction side)
+        {
+            // call methods to make link pick up bow
+            // call methods to despawn bow
+        }
+    }
+}
