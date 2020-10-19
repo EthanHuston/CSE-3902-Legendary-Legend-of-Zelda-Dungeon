4f5ec6b1b4898902ba4b9b48f93871c0a9bb45a5
Ethan Huston <ethanghuston@gmail.com>
2020-10-18 21:06:43 -0400

    Implemented more Block Colliders

--- LegendOfZelda/Enemies/CollisionHandlers/SkeletonBlockCollisionHandler.cs
+++ LegendOfZelda/Enemies/CollisionHandlers/SkeletonBlockCollisionHandler.cs
@@ -1,4 +1,5 @@
 ﻿using LegendOfZelda.Interface;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,29 @@ namespace LegendOfZelda.Enemies.CollisionHandlers
     {
         public void HandleCollison(INpc enemy, IBlock block, Constants.Direction side)
         {
+            Vector2 correctDirection;
+            switch (side)
+            {
+                case Constants.Direction.Up:
+                    correctDirection = new Vector2(Constants.EnemyNoMove, Constants.EnemyMoveUp);
+                    enemy.Move(correctDirection);
+                    break;
+                case Constants.Direction.Down:
+                    correctDirection = new Vector2(Constants.EnemyNoMove, Constants.EnemyMoveDown);
+                    enemy.Move(correctDirection);
+                    break;
+                case Constants.Direction.Left:
+                    correctDirection = new Vector2(Constants.EnemyMoveLeft, Constants.EnemyNoMove);
+                    enemy.Move(correctDirection);
+                    break;
+                case Constants.Direction.Right:
+                    correctDirection = new Vector2(Constants.EnemyMoveRight, Constants.EnemyNoMove);
+                    enemy.Move(correctDirection);
+                    break;
+                default:
+                    break;
+
+            }
 
         }
     }
