516e0892f5da0d34a9a373c8fbe3bceb57098d42
Nathan Murphy <murphy.1769@buckeyemail.osu.edu>
2020-10-18 21:32:33 -0400

    AB#163 Created Map to Link collision

---
+++ LegendOfZelda/Link/CollisionHandler/LinkMapItemCollisionHandler.cs
@@ -0,0 +1,13 @@
+﻿using LegendOfZelda.Interface;
+
+namespace LegendOfZelda.Link.CollisionHandler
+{
+    class LinkMapItemCollisionHandler : ICollision<IPlayer, IItem>
+    {
+        public void HandleCollison(IPlayer link, IItem map, Constants.Direction side)
+        {
+            link.PickupMap();
+            map.Despawn();
+        }
+    }
+}
