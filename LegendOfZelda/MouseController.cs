a7f09d916491652e1d68f6a437cd8f57cd3082ca
chris <chris101112@gmail.com>
2020-10-22 23:59:52 -0400

    Created Mouse controller AB#222

---
+++ LegendOfZelda/MouseController.cs
@@ -0,0 +1,32 @@
+﻿using LegendOfZelda.Interface;
+using LegendOfZelda.Link.Command;
+using LegendOfZelda.Sprint2;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace LegendOfZelda
+{
+    public class MouseController : IController
+    {
+        private Game1 myGame;
+        private Dictionary<Keys, ICommand> controllerMappings;
+
+        public MouseController(Game1 game1)
+        {
+            myGame = game1;
+            controllerMappings = new Dictionary<Keys, ICommand>();
+            
+        }
+
+        public void RegisterCommand(Keys key, ICommand command)
+        {
+            controllerMappings.Add(key, command);
+        }
+
+        public void Update()
+        {
+            
+        }
+
+    }
+}
