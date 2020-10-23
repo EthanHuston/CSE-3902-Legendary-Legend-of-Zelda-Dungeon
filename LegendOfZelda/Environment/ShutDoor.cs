fdaecce3fbaf2d3624eac86ad2d0f0a427e1eb84
chris <chris101112@gmail.com>
2020-10-23 12:34:34 -0400

    Fixed the rest of the doors and wall pieces

--- LegendOfZelda/Environment/ShutDoor.cs
+++ LegendOfZelda/Environment/ShutDoor.cs
@@ -10,8 +10,8 @@ namespace LegendOfZelda.Environment
         private ITextureAtlasSprite doorSprite;
         private SpriteBatch sB;
         private bool safeToDespawn;
-        private const int textureAtlasColumn = 3;
-        private int textureAtlasRow;
+        private const int textureMapColumn = 2;
+        private int textureMapRow;
 
         private Point position;
         public Point Position { get => new Point(position.X, position.Y); set => position = new Point(value.X, value.Y); }
@@ -26,23 +26,23 @@ namespace LegendOfZelda.Environment
 
         public void Draw()
         {
-            if ((position.X != Constants.MinXPos) && (position.Y == Constants.MinYPos))
+            if ((position.X == RoomConstants.topDoorX) && (position.Y == RoomConstants.topDoorY))
             {
-                textureAtlasRow = 1;
+                textureMapRow = 0;
             }
-            else if ((position.X == Constants.MinXPos) && (position.Y != Constants.MinYPos))
+            else if ((position.X == RoomConstants.leftDoorX) && (position.Y == RoomConstants.leftDoorY))
             {
-                textureAtlasRow = 2;
+                textureMapRow = 1;
             }
-            else if ((position.X == Constants.MaxXPos) && (position.Y != Constants.MinYPos))
+            else if ((position.X == RoomConstants.rightDoorX) && (position.Y == RoomConstants.rightDoorY))
             {
-                textureAtlasRow = 3;
+                textureMapRow = 2;
             }
-            else if ((position.X != Constants.MinXPos) && (position.Y == Constants.MaxYPos))
+            else if ((position.X == RoomConstants.bottomDoorX) && (position.Y == RoomConstants.bottomDoorY))
             {
-                textureAtlasRow = 4;
+                textureMapRow = 3;
             }
-            doorSprite.Draw(sB, position, new Point(textureAtlasColumn, textureAtlasRow));
+            doorSprite.Draw(sB, position, new Point(textureMapColumn, textureMapRow));
         }
 
         public void Update()
