3c9b3e5e5c8e9f627293c5353b05ec3cef4e6ec3
chris <chris101112@gmail.com>
2020-10-23 00:07:12 -0400

    Started room commands AB#222

---
+++ LegendOfZelda/Rooms/Commands/ChangeRoomCommand.cs
@@ -0,0 +1,20 @@
+﻿
+using LegendOfZelda.Interface;
+using LegendOfZelda.Rooms;
+using Microsoft.Xna.Framework;
+
+namespace LegendOfZelda.Link.Command
+{
+    class ChangeRoomCommand : ICommand
+    {
+        RoomManager rooms;
+        public ChangeRoomCommand(RoomManager rooms, Point mousePosition)
+        {
+            this.rooms = rooms;
+        }
+        public void Execute()
+        {
+            
+        }
+    }
+}
