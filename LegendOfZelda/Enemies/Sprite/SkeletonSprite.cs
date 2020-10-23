6a64c464ff193f841b0de0cb519ca8620735d187
chris <chris101112@gmail.com>
2020-10-23 14:40:06 -0400

    removed spriteBatch.Begin() from skeletonSprite

--- LegendOfZelda/Enemies/Sprite/SkeletonSprite.cs
+++ LegendOfZelda/Enemies/Sprite/SkeletonSprite.cs
@@ -53,7 +53,6 @@ namespace LegendOfZelda.Enemies.Sprite
             sourceRectangle = new Rectangle(width * column, height * row, width, height);
             destinationRectangle = new Rectangle(position.X, position.Y, spriteScaler * width, spriteScaler * height);
 
-            spriteBatch.Begin();
             if (damaged)
             {
                 spriteBatch.Draw(sprite, destinationRectangle, sourceRectangle, Color.Red);
@@ -65,7 +64,6 @@ namespace LegendOfZelda.Enemies.Sprite
                 spriteBatch.Draw(sprite, destinationRectangle, sourceRectangle, Color.White);
 
             }
-            spriteBatch.End();
         }
         public Rectangle GetPositionRectangle()
         {
