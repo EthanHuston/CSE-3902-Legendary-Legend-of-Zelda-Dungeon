2ddc53e2782932c0a35ef00d25b3b4dc424be079
chris <chris101112@gmail.com>
2020-10-23 01:28:28 -0400

    Implemented mouse controller

--- LegendOfZelda/MouseController.cs
+++ LegendOfZelda/MouseController.cs
@@ -17,8 +17,10 @@ namespace LegendOfZelda
         {
             myGame = game1;
             leftClickCommands = new Dictionary<Constants.Direction, ICommand>();
-            RegisterCommand(Constants.Direction.Down, new ChangeRoomDownCommand(((RoomGameState)myGame.State).roomManager));
-            
+            RegisterCommand(Constants.Direction.Down, new ChangeRoomDownCommand(((RoomGameState)myGame.State).RoomManager));
+            RegisterCommand(Constants.Direction.Left, new ChangeRoomLeftCommand(((RoomGameState)myGame.State).RoomManager));
+            RegisterCommand(Constants.Direction.Right, new ChangeRoomRightCommand(((RoomGameState)myGame.State).RoomManager));
+            RegisterCommand(Constants.Direction.Up, new ChangeRoomUpCommand(((RoomGameState)myGame.State).RoomManager));
         }
 
         public void RegisterCommand(Constants.Direction dir, ICommand command)
@@ -28,12 +30,41 @@ namespace LegendOfZelda
 
         public void Update()
         {
-            
+            MouseState state = Mouse.GetState();
+            if(state.LeftButton == ButtonState.Pressed)
+            {
+                Constants.Direction dir = GetDirectionFromClick(state.Position);
+                if (leftClickCommands.ContainsKey(dir))
+                    leftClickCommands[dir].Execute();
+            }
         }
 
         private Point GetLocation()
         {
             return Mouse.GetState().Position;
+        }
+
+        private Constants.Direction GetDirectionFromClick(Point mousePos)
+        {
+            Constants.Direction dir;
+            
+            if (mousePos.X > RoomConstants.topDoorX && mousePos.X < RoomConstants.topDoorX + RoomConstants.wallWidth)
+            {
+                if (mousePos.Y > RoomConstants.topDoorY && mousePos.Y < RoomConstants.topDoorY + RoomConstants.wallWidth)
+                    dir = Constants.Direction.Up;
+                else if (mousePos.Y > RoomConstants.bottomDoorY && mousePos.Y < RoomConstants.bottomDoorY + RoomConstants.wallWidth)
+                    dir = Constants.Direction.Down;
+            }
+
+            else if (mousePos.Y > RoomConstants.leftDoorY && mousePos.Y < RoomConstants.leftDoorY + RoomConstants.wallWidth)
+            {
+                if (mousePos.X >= RoomConstants.leftDoorX && mousePos.X <= RoomConstants.leftDoorX + RoomConstants.wallWidth)
+                    dir = Constants.Direction.Left;
+                else if (mousePos.X >= RoomConstants.rightDoorX && mousePos.X <= RoomConstants.rightDoorX + RoomConstants.wallWidth)
+                    dir = Constants.Direction.Right;
+            }
+
+            return dir;
         }
 
     }
