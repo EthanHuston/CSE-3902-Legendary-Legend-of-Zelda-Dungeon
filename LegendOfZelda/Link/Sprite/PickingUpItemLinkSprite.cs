6543cf9a43c7ac50dad6ebed1b2f1e5afb3d43b5
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-10-30 15:44:58 -0400

    Fixed bad get position rectangle method

--- LegendOfZelda/Link/Sprite/PickingUpItemLinkSprite.cs
+++ LegendOfZelda/Link/Sprite/PickingUpItemLinkSprite.cs
@@ -11,7 +11,6 @@ namespace LegendOfZelda.Link.Sprite
         private bool flashRed;
         private int damageColorCounter;
         private int delayCounter;
-        private Rectangle destinationRectangle;
 
         public PickingUpItemLinkSprite(Texture2D sprite)
         {
@@ -20,7 +19,6 @@ namespace LegendOfZelda.Link.Sprite
             flashRed = false;
             damageColorCounter = 0;
             delayCounter = 0;
-            destinationRectangle = Rectangle.Empty;
         }
 
         public void Update()
@@ -43,7 +41,7 @@ namespace LegendOfZelda.Link.Sprite
 
         public void Draw(SpriteBatch spriteBatch, Point position, bool drawWithDamage)
         {
-            destinationRectangle = new Rectangle(position.X, position.Y, sprite.Width, sprite.Height);
+            Rectangle destinationRectangle = new Rectangle(position.X, position.Y, sprite.Width, sprite.Height);
 
             spriteBatch.Draw(sprite, destinationRectangle, flashRed && drawWithDamage ? Color.Red : Color.White);
         }
@@ -55,7 +53,7 @@ namespace LegendOfZelda.Link.Sprite
 
         public Rectangle GetPositionRectangle()
         {
-            return destinationRectangle;
+            return new Rectangle(0, 0, sprite.Width, sprite.Height);
         }
     }
 }
