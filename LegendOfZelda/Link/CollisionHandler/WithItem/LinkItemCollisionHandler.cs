83ab9c3926cdf144018a22625a7ee873e4b78c16
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-10-30 19:14:06 -0400

    Combined item collision handlers into one class

---
+++ LegendOfZelda/Link/CollisionHandler/WithItem/LinkItemCollisionHandler.cs
@@ -0,0 +1,14 @@
+﻿using LegendOfZelda.GameLogic;
+using LegendOfZelda.Item;
+
+namespace LegendOfZelda.Link.CollisionHandler.WithItem
+{
+    class LinkItemCollisionHandler : ICollisionHandler<IPlayer, IItem>
+    {
+        public void HandleCollision(IPlayer link, IItem item, Constants.Direction side)
+        {
+            link.PickupItem(item.GetItemType());
+            item.Despawn();
+        }
+    }
+}
