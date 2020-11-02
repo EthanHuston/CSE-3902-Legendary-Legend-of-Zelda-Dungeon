26e6c4439cf90640ea5d918fd8ea01b6cb57fe47
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-11-01 19:14:03 -0500

    Created command to exit game and return to main menu
    AB#262

---
+++ LegendOfZelda/GameState/Pause/Command/MainMenuCommand.cs
@@ -0,0 +1,24 @@
+﻿using LegendOfZelda.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LegendOfZelda.GameState.Pause.Command
+{
+    class MainMenuCommand : ICommand
+    {
+        private IGameState gameState;
+
+        public MainMenuCommand(IGameState gameState)
+        {
+            this.gameState = gameState;
+        }
+
+        public void Execute()
+        {
+            gameState.SwitchToMainMenuState();
+        }
+    }
+}
