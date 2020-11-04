c6bfb7113a95f6fb6afaf1bf190a8b7bf5cc485d
chris <chris101112@gmail.com>
2020-11-04 17:07:00 -0500

    created HUD class.

---
+++ LegendOfZelda/HUD/HUD.cs
@@ -0,0 +1,20 @@
+﻿using LegendOfZelda.GameLogic;
+using LegendOfZelda.GameState.Pause;
+using LegendOfZelda.Link;
+using LegendOfZelda.Link.Interface;
+using LegendOfZelda.Rooms;
+using LegendOfZelda.Utility;
+using System.Collections.Generic;
+
+namespace LegendOfZelda.HUD
+{
+    internal class HUD
+    {
+        List<IPlayer> players;
+
+        public HUD(List<IPlayer> players)
+        {
+            this.players = players;
+        }
+    }
+}
---
+++ LegendOfZelda/HUD/Sprites/HUDSprite.cs
@@ -0,0 +1,34 @@
+﻿using LegendOfZelda.Interface;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace LegendOfZelda.GameState.Sprite
+{
+    class HUDSprite : ISprite
+    {
+        private Texture2D sprite;
+
+        public HUDSprite(Texture2D sprite)
+        {
+            this.sprite = sprite;
+        }
+
+        public void Update()
+        {
+            // No update needed for an unchanging object.
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Point position)
+        {
+            Rectangle destinationRectangle = new Rectangle(position.X, position.Y, (int)(RoomConstants.SpriteMultiplier * sprite.Width), (int)(RoomConstants.SpriteMultiplier * sprite.Height));
+            Rectangle sourceRectangle = new Rectangle(0, 0, sprite.Width, sprite.Height);
+            spriteBatch.Draw(sprite, destinationRectangle, sourceRectangle, Color.White);
+        }
+
+        public Rectangle GetPositionRectangle()
+        {
+            return sprite.Bounds;
+        }
+    }
+}
