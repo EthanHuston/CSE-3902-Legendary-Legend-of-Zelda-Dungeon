d45aac0efc2cc3f81c43c119bd937ee87751919b
chris <chris101112@gmail.com>
2020-11-06 19:15:48 -0500

    Added bomb numbers to HUD

--- LegendOfZelda/HUDClasses/HUD.cs
+++ LegendOfZelda/HUDClasses/HUD.cs
@@ -1,12 +1,7 @@
-﻿using LegendOfZelda.GameLogic;
-using LegendOfZelda.GameState;
-using LegendOfZelda.GameState.Pause;
-using LegendOfZelda.HUDClasses.Sprite;
+﻿using LegendOfZelda.GameState;
 using LegendOfZelda.Interface;
 using LegendOfZelda.Link;
 using LegendOfZelda.Link.Interface;
-using LegendOfZelda.Rooms;
-using LegendOfZelda.Utility;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
@@ -15,16 +10,18 @@ namespace LegendOfZelda.HUDClasses
 {
     internal class HUD
     {
-        List<IPlayer> players;
-        ISprite hudSprite;
-        Point position = new Point(HUDConstants.hudx, HUDConstants.hudy);
-        HUDNumber levelNum;
-        HUDNumber[] numRupees;
-        int linkRupeeCount;
-        HUDNumber[] numKeys;
-        int linkKeyCount;
-        ISprite minimapSprite;
-        bool displayMinimap;
+        private List<IPlayer> players;
+        private ISprite hudSprite;
+        private Point position = new Point(HUDConstants.hudx, HUDConstants.hudy);
+        private HUDNumber[] numRupees;
+        private HUDNumber[] numKeys;
+        private HUDNumber[] numBombs;
+        private int linkRupeeCount = -20; //These will be changed on the first update
+        private int linkKeyCount = -20;
+        private int linkBombCount = -20;
+        private HUDNumber levelNum;
+        private ISprite minimapSprite;
+        private bool displayMinimap;
 
         public HUD(List<IPlayer> players)
         {
@@ -32,20 +29,27 @@ namespace LegendOfZelda.HUDClasses
             hudSprite = HUDSpriteFactory.Instance.CreateHUDSprite();
             minimapSprite = HUDSpriteFactory.Instance.CreateMiniMapSprite();
             levelNum = new HUDNumber(1);
-            numRupees = new HUDNumber[3];
-            numKeys = new HUDNumber[3];
             initNumberArrays();
             displayMinimap = false;
-            linkRupeeCount = -20;
-            linkKeyCount = -20;
         }
 
         private void initNumberArrays()
         {
-            for(int i = 0; i < 3; i++)
+            numRupees = new HUDNumber[3];
+            numKeys = new HUDNumber[3];
+            numBombs = new HUDNumber[3];
+
+            //-1 assings the HUDNumber to the "X"
+            numRupees[0] = new HUDNumber(-1);
+            numKeys[0] = new HUDNumber(-1);
+            numBombs[0] = new HUDNumber(-1);
+
+            for(int i = 1; i < 3; i++)
             {
+                //10 assigns the HUDNumber to the black square
                 numRupees[i] = new HUDNumber(10);
                 numKeys[i] = new HUDNumber(10);
+                numBombs[i] = new HUDNumber(10);
             }
         }
 
@@ -53,8 +57,12 @@ namespace LegendOfZelda.HUDClasses
         {
             if (players[0].GetQuantityInInventory(LinkConstants.ItemType.Map) != 0)
                 displayMinimap = true;
-            if(players[0].GetQuantityInInventory(LinkConstants.ItemType.Rupee) != linkRupeeCount)
+            if (players[0].GetQuantityInInventory(LinkConstants.ItemType.Rupee) != linkRupeeCount)
                 UpdateNumRupees();
+            if (players[0].GetQuantityInInventory(LinkConstants.ItemType.Key) != linkKeyCount)
+                UpdateNumKeys();
+            if (players[0].GetQuantityInInventory(LinkConstants.ItemType.Bomb) != linkBombCount)
+                UpdateNumBombs();
         }
 
         public void Draw(SpriteBatch spriteBatch)
@@ -64,6 +72,8 @@ namespace LegendOfZelda.HUDClasses
             if (displayMinimap)
                 minimapSprite.Draw(spriteBatch, GameStateConstants.MinimapLocation);
             DrawNumRupees(spriteBatch);
+            DrawNumKeys(spriteBatch);
+            DrawNumBombs(spriteBatch);
         }
 
         private void DrawNumRupees(SpriteBatch spriteBatch)
@@ -74,14 +84,28 @@ namespace LegendOfZelda.HUDClasses
                 numRupees[i].Draw(spriteBatch, position);
             }
         }
+        private void DrawNumKeys(SpriteBatch spriteBatch)
+        {
+            for (int i = 0; i < numKeys.Length; i++)
+            {
+                Point position = new Point(HUDConstants.KeyNumberX + i * HUDConstants.NumberWidth, HUDConstants.KeyNumberY);
+                numKeys[i].Draw(spriteBatch, position);
+            }
+        }
+        private void DrawNumBombs(SpriteBatch spriteBatch)
+        {
+            for (int i = 0; i < numBombs.Length; i++)
+            {
+                Point position = new Point(HUDConstants.BombNumberX + i * HUDConstants.NumberWidth, HUDConstants.BombNumberY);
+                numBombs[i].Draw(spriteBatch, position);
+            }
+        }
 
         private void UpdateNumRupees()
         {
-            numRupees[0].AssignNumber(-1);
             linkRupeeCount = players[0].GetQuantityInInventory(LinkConstants.ItemType.Rupee);
-            int tensPlace = linkRupeeCount;
-            int remainder = tensPlace % 10;
-            tensPlace /= 10;
+            int tensPlace = linkRupeeCount / 10;
+            int remainder = linkRupeeCount % 10;
             if (tensPlace > 0)
             {
                 numRupees[1].AssignNumber(tensPlace);
@@ -91,6 +115,40 @@ namespace LegendOfZelda.HUDClasses
             {
                 numRupees[1].AssignNumber(remainder);
                 numRupees[2].AssignNumber(10);
+            }
+        }
+
+        private void UpdateNumKeys()
+        {
+            linkKeyCount = players[0].GetQuantityInInventory(LinkConstants.ItemType.Key);
+            int tensPlace = linkKeyCount / 10;
+            int remainder = linkKeyCount % 10;
+            if (tensPlace > 0)
+            {
+                numKeys[1].AssignNumber(tensPlace);
+                numKeys[2].AssignNumber(remainder);
+            }
+            else
+            {
+                numKeys[1].AssignNumber(remainder);
+                numKeys[2].AssignNumber(10);
+            }
+        }
+
+        private void UpdateNumBombs()
+        {
+            linkBombCount = players[0].GetQuantityInInventory(LinkConstants.ItemType.Bomb);
+            int tensPlace = linkBombCount / 10;
+            int remainder = linkBombCount % 10;
+            if (tensPlace > 0)
+            {
+                numBombs[1].AssignNumber(tensPlace);
+                numBombs[2].AssignNumber(remainder);
+            }
+            else
+            {
+                numBombs[1].AssignNumber(remainder);
+                numBombs[2].AssignNumber(10);
             }
         }
     }
--- LegendOfZelda/HUDClasses/HUDConstants.cs
+++ LegendOfZelda/HUDClasses/HUDConstants.cs
@@ -11,6 +11,10 @@ namespace LegendOfZelda.HUDClasses
         //Rupee Number Locations
         public const int RupeeNumberX = (int)(96 * Constants.GameScaler);
         public const int RupeeNumberY = (int)(16 * Constants.GameScaler);
+        public const int KeyNumberX = (int)(96 * Constants.GameScaler);
+        public const int KeyNumberY = (int)(32 * Constants.GameScaler);
+        public const int BombNumberX = (int)(96 * Constants.GameScaler);
+        public const int BombNumberY = (int)(40 * Constants.GameScaler);
         public const int NumberWidth = (int)(8 * Constants.GameScaler);
 
     }
