eba7521e097406130a01a69efd6a5631abedf401
Ethan Huston <ethanghuston@gmail.com>
2020-11-08 22:08:05 -0500

    Adding classes

---
+++ LegendOfZelda/GameState/GameWinState/GameWinState.cs
@@ -0,0 +1,11 @@
+﻿using LegendOfZelda.GameState.Button;
+using LegendOfZelda.GameState.MainMenu;
+using LegendOfZelda.Interface;
+using System.Collections.Generic;
+
+namespace LegendOfZelda.GameState.GameLoseState
+{
+    class GameWinState : AbstractGameState
+    {
+    }
+}
