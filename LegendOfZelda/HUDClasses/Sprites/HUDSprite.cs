f6616a295a730f635065de7e5835fb0dc822396c
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-11-10 11:08:02 -0500

    Fixed readonly warning

--- LegendOfZelda/HUDClasses/Sprites/HUDSprite.cs
+++ LegendOfZelda/HUDClasses/Sprites/HUDSprite.cs
@@ -7,13 +7,11 @@ namespace LegendOfZelda.HUDClasses.Sprite
 {
     class HUDSprite : ISprite
     {
-        private Texture2D sprite;
-        private Rectangle destinationRectangle;
+        private readonly Texture2D sprite;
 
         public HUDSprite(Texture2D sprite)
         {
             this.sprite = sprite;
-            destinationRectangle = sprite.Bounds;
         }
 
         public void Update()
@@ -23,14 +21,14 @@ namespace LegendOfZelda.HUDClasses.Sprite
 
         public void Draw(SpriteBatch spriteBatch, Point position)
         {
-            destinationRectangle = new Rectangle(position.X, position.Y, (int)(Constants.GameScaler * sprite.Width), (int)(Constants.GameScaler * sprite.Height));
+            Rectangle destinationRectangle = new Rectangle(position.X, position.Y, (int)(Constants.GameScaler * sprite.Width), (int)(Constants.GameScaler * sprite.Height));
             Rectangle sourceRectangle = new Rectangle(0, 0, sprite.Width, sprite.Height);
             spriteBatch.Draw(sprite, destinationRectangle, sourceRectangle, Color.White);
         }
 
         public Rectangle GetPositionRectangle()
         {
-            return destinationRectangle;
+            return new Rectangle(0,0,(int)(sprite.Width * Constants.GameScaler),(int)(sprite.Height * Constants.GameScaler));
         }
     }
 }
