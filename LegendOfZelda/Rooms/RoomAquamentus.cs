964ed8e76afd6c03d081d3ee8f5a316f66f901e4
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-11-13 17:39:49 -0500

    Fixed aquamentus door so it now opens when he dies

---
+++ LegendOfZelda/Rooms/RoomAquamentus.cs
@@ -0,0 +1,26 @@
+﻿using LegendOfZelda.Link.Interface;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace LegendOfZelda.Rooms
+{
+    class RoomAquamentus : Room
+    {
+        bool openedDoor;
+        public RoomAquamentus(SpriteBatch spriteBatch, string fileName, List<IPlayer> playerList) : base(spriteBatch, fileName, playerList)
+        {
+            openedDoor = false;
+        }
+
+
+        public override void Update()
+        {
+            if (!openedDoor && AllObjects.NpcList.Count == 0)
+            {
+                GetDoor(Constants.Direction.Right).OpenDoor();
+                openedDoor = true;
+            }
+            base.Update();
+        }
+    }
+}
