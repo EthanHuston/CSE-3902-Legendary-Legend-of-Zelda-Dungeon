2dfdb8a58591058d9178319c7432dc130893cbdf
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-11-16 09:59:53 -0500

    adjusted sword hitbox

--- LegendOfZelda/Projectile/Sprite/SwordAttackingSprite.cs
+++ LegendOfZelda/Projectile/Sprite/SwordAttackingSprite.cs
@@ -57,7 +57,7 @@ namespace LegendOfZelda.Projectile.Sprite
 
         public Rectangle GetPositionRectangle()
         {
-            return new Rectangle(0, 0, (int) (frameWidth * Constants.GameScaler), (int) (frameHeight * Constants.GameScaler));
+            return new Rectangle(0, 0, (int)(frameWidth * Constants.GameScaler) - LinkConstants.SwordHitboxShortener, (int)(frameHeight * Constants.GameScaler) - LinkConstants.SwordHitboxShortener);
         }
     }
 }
