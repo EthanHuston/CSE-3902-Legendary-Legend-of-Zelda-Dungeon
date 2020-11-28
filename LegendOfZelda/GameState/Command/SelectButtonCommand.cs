f22cd1fa395818f74993a3dce98ba74a6f30f300
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-11-28 10:46:56 -0500

    Added a select button command

--- LegendOfZelda/GameState/Command/SelectButtonCommand.cs
+++ LegendOfZelda/GameState/Command/SelectButtonCommand.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using LegendOfZelda.Interface;
+using LegendOfZelda.Menu;
+
 namespace LegendOfZelda.GameState.Command
 {
-    public class SelectButtonCommand
+    class SelectButtonCommand : ICommand
     {
-        public SelectButtonCommand()
+        private readonly ButtonSelector buttonSelector;
+        private readonly Dictionary<Type, ICommand> buttonCommands;
+
+        public SelectButtonCommand(ButtonSelector buttonSelector, Dictionary<Type, ICommand> buttonCommands)
         {
+            this.buttonSelector = buttonSelector;
+            this.buttonCommands = buttonCommands;
+        }
+
+        public void Execute()
+        {
+            buttonCommands[buttonSelector.ButtonSelected.GetType()].Execute();
         }
     }
 }
