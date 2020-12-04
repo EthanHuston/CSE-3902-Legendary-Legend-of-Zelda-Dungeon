88fa1fccb375e947100e1799741af64d258b3fe8
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-12-04 12:07:41 -0500

    added command to change to pokemon sprites

---
+++ LegendOfZelda/GameState/Command/ChangeToPokemonSpritesCommand.cs
@@ -0,0 +1,19 @@
+﻿using LegendOfZelda.Interface;
+
+namespace LegendOfZelda.GameState.Command
+{
+    class ChangeToPokemonSpritesCommand : ICommand
+    {
+        private readonly IGameState gameState;
+
+        public ChangeToPokemonSpritesCommand(IGameState gameState)
+        {
+            this.gameState = gameState;
+        }
+
+        public void Execute()
+        {
+            // i'm guessing we'll probably toggle a bool in option game state here?? might need to cast gameState to OptionGameState
+        }
+    }
+}
