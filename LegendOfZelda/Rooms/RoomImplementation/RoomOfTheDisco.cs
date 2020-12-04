779b224f09b10afd03e9f3b64307dea8193c73ee
Ryan Stuckey <ryanstuckey0@gmail.com>
2020-12-04 11:46:50 -0500

    Made dungeon music a public member

---
+++ LegendOfZelda/Rooms/RoomImplementation/RoomOfTheDisco.cs
@@ -0,0 +1,16 @@
+﻿using LegendOfZelda.Link.Interface;
+using System.Collections.Generic;
+
+namespace LegendOfZelda.Rooms.RoomImplementation
+{
+    class RoomOfTheDisco : Room
+    {
+        public RoomOfTheDisco(List<IPlayer> playerList, Game1 game) : base(playerList, game)
+        { }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+    }
+}
