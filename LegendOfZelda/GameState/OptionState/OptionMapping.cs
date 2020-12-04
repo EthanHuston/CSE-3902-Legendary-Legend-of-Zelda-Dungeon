298599f271d18e1299ed35c41113bc74b6513981
Nathan Murphy <murphy.1769@buckeyemail.osu.edu>
2020-12-03 21:14:47 -0500

    AB#313 Option Menu created.

---
+++ LegendOfZelda/GameState/OptionState/OptionMapping.cs
@@ -0,0 +1,82 @@
+﻿using LegendOfZelda.GameState.Button;
+using LegendOfZelda.GameState.Command;
+using LegendOfZelda.GameState.Utilities;
+using LegendOfZelda.Interface;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace LegendOfZelda.GameState.OptionState
+{
+    class OptionMapping : IGameStateControllerMappings
+    {
+        public Dictionary<Keys, ICommand> KeyboardMappings { get; private set; }
+        public Dictionary<MouseButton, ICommand> MouseMappings { get; private set; }
+        public Dictionary<Buttons, ICommand> GamepadMappings { get; private set; }
+        public Dictionary<Type, ICommand> ButtonMappings { get; private set; }
+        public List<Keys> RepeatableKeyboardKeys { get; private set; }
+        public List<Buttons> RepeatableGamepadButtons { get; private set; }
+
+        public OptionMapping(IGameState gameState)
+        {
+            GamepadMappings = GetGamepadMappings(gameState);
+            KeyboardMappings = GetKeyboardMappings(gameState);
+            MouseMappings = GetMouseMappings(gameState);
+            ButtonMappings = GetButtonMappings(gameState);
+            RepeatableKeyboardKeys = GetRepeatableKeyboardKeys();
+            RepeatableGamepadButtons = GetRepeatableGamepadButtons();
+        }
+
+        private Dictionary<Buttons, ICommand> GetGamepadMappings(IGameState gameState)
+        {
+            OptionState gameStateCast = (OptionState)gameState;
+            ICommand resumeGameCommand = new ResumeGameCommand(gameState);
+            return new Dictionary<Buttons, ICommand>
+            {
+                { Buttons.Start, resumeGameCommand },
+                { Buttons.B, resumeGameCommand },
+                { Buttons.LeftThumbstickUp, new MoveSelectorCommand(gameStateCast.OptionMenu, Constants.Direction.Up) },
+                { Buttons.LeftThumbstickRight, new MoveSelectorCommand(gameStateCast.OptionMenu, Constants.Direction.Right) },
+                { Buttons.LeftThumbstickDown, new MoveSelectorCommand(gameStateCast.OptionMenu, Constants.Direction.Down) },
+                { Buttons.LeftThumbstickLeft, new MoveSelectorCommand(gameStateCast.OptionMenu, Constants.Direction.Left) },
+                { Buttons.A, new SelectButtonCommand(gameStateCast.OptionMenu.ButtonSelector, GetButtonMappings(gameState)) }
+            };
+        }
+
+        public Dictionary<Keys, ICommand> GetKeyboardMappings(IGameState gameState)
+        {
+            OptionState gameStateCast = (OptionState)gameState;
+            return new Dictionary<Keys, ICommand>
+            {
+                { Keys.Escape, new ResumeGameCommand(gameState) },
+                { Keys.W, new MoveSelectorCommand(gameStateCast.OptionMenu, Constants.Direction.Up) },
+                { Keys.D, new MoveSelectorCommand(gameStateCast.OptionMenu, Constants.Direction.Right) },
+                { Keys.S, new MoveSelectorCommand(gameStateCast.OptionMenu, Constants.Direction.Down) },
+                { Keys.A, new MoveSelectorCommand(gameStateCast.OptionMenu, Constants.Direction.Left) },
+                { Keys.Enter, new SelectButtonCommand(gameStateCast.OptionMenu.ButtonSelector, GetButtonMappings(gameState)) }
+            };
+        }
+
+        private Dictionary<Type, ICommand> GetButtonMappings(IGameState gameState)
+        {
+            return new Dictionary<Type, ICommand>
+            {
+                {typeof(ResumeButton), new ResumeGameCommand(gameState) },
+                {typeof(MainMenuButton), new MainMenuCommand(gameState) },
+                {typeof(ExitButton), new ExitGameCommand(gameState) }
+            };
+        }
+
+        private Dictionary<MouseButton, ICommand> GetMouseMappings(IGameState gameState)
+        {
+            return new Dictionary<MouseButton, ICommand>
+            {
+                {MouseButton.LeftButton, new StartGameCommand(gameState) }
+            };
+        }
+
+        private List<Keys> GetRepeatableKeyboardKeys() { return new List<Keys>(); }
+
+        private List<Buttons> GetRepeatableGamepadButtons() { return new List<Buttons>(); }
+    }
+}
---
+++ LegendOfZelda/GameState/OptionState/OptionState.cs
@@ -0,0 +1,87 @@
+﻿using LegendOfZelda.GameState.Controller;
+using LegendOfZelda.GameState.MainMenuState;
+using LegendOfZelda.GameState.Utilities;
+using LegendOfZelda.Menu;
+using System.Collections.Generic;
+
+namespace LegendOfZelda.GameState.OptionState
+{
+    class OptionState : IGameState
+    {
+        private readonly List<IController> controllerList;
+        private readonly IGameState roomStatePreserved;
+
+        public Game1 Game { get; private set; }
+        public IButtonMenu PauseGameMenu { get; private set; }
+
+        public OptionState(Game1 game, IGameState oldRoomState)
+        {
+            Game = game;
+            roomStatePreserved = oldRoomState;
+            PauseGameMenu = new PauseGameMenu(Game);
+            controllerList = GetControllerList();
+        }
+
+        private List<IController> GetControllerList()
+        {
+            IGameStateControllerMappings mappings = new OptionMapping(this);
+            return new List<IController>()
+            {
+                {new KeyboardController(mappings.KeyboardMappings, mappings.RepeatableKeyboardKeys) },
+                {new MouseController(mappings.MouseMappings, mappings.ButtonMappings, PauseGameMenu.Buttons) },
+                {new GamepadController(mappings.GamepadMappings, mappings.RepeatableGamepadButtons) }
+            };
+        }
+
+        public void Draw()
+        {
+            roomStatePreserved.Draw(); // continue to draw the old room in the background
+            PauseGameMenu.Draw();
+        }
+
+        public void SwitchToRoomState()
+        {
+            StateExitProcedure();
+            Game.State = roomStatePreserved;
+            Game.State.SetControllerOldInputState(GameStateMethods.GetOldInputState(controllerList));
+            Game.State.StateEntryProcedure();
+        }
+
+        public void SwitchToMainMenuState()
+        {
+            StateExitProcedure();
+            Game.State = new MainMenuGameState(Game);
+            Game.State.SetControllerOldInputState(GameStateMethods.GetOldInputState(controllerList));
+            Game.State.StateEntryProcedure();
+        }
+
+        public void StateEntryProcedure()
+        {
+            // nothing fancy to do here
+        }
+
+        public void StateExitProcedure()
+        {
+            // nothing fancy to do here
+        }
+
+        public void Update()
+        {
+            foreach (IController controller in controllerList) controller.Update();
+            PauseGameMenu.Update();
+        }
+
+        public void SwitchToPauseState() { }
+
+        public void SwitchToItemSelectionState() { }
+
+        public void SwitchToDeathState() { }
+
+        public void SwitchToWinState() { }
+
+        public void SetControllerOldInputState(InputStates inputFromOldState)
+        {
+            foreach (IController controller in controllerList) controller.OldInputState = inputFromOldState;
+        }
+    }
+}
